45763f2621fe939084cdefd7d6f38cb41e5ab1ac
Michael Buffone <michaelbuffonee@gmail.com>
2019-11-08 18:08:21 -0500

    +

--- Assignments/Assignment 4/src/Calculator.java
+++ Assignments/Assignment 4/src/Calculator.java
@@ -5,8 +5,10 @@
  * 
  * 	JavaFX calculator
  */
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -15,6 +17,13 @@ import javafx.stage.Stage;
 
 public class Calculator extends Application {
 
+	
+	/*--------------------Pane object declaration--------------------*/
+	// Infix / Postfix radio buttons
+	ToggleGroup tgFixGroup = new ToggleGroup();
+	RadioButton rbInfix = new RadioButton("Infix");
+	RadioButton rbPostfix = new RadioButton("Postfix");
+	
 	// Operator buttons
 	private TextField calcArea = new TextField();
 	private Button btClear = new Button("C");
@@ -25,7 +34,6 @@ public class Calculator extends Application {
 	private Button btEquals = new Button("=");
 	private Button btLeftBracket = new Button("(");
 	private Button btRightBracket = new Button(")");
-
 
 	// Number buttons
 	private Button btOne = new Button("1");
@@ -41,22 +49,48 @@ public class Calculator extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-				
-		calcArea.setPromptText("Ex. 1+5/6*2");
-		calcArea.setMaxSize(500, 500);
 		
+		// Main pane
 		BorderPane mainPane = new BorderPane();
-		mainPane.setPadding(new Insets(10, 10, 10, 10));
-	
+		mainPane.setPadding(new Insets(20, 20, 20, 20));
 		mainPane.setTop(calcArea);
 		
-
+		// Text box
+		calcArea.setPromptText("Ex. 1+5/6*2");
+		calcArea.setPrefSize(400, 40);
+		calcArea.setStyle("-fx-font-size: 12pt;");
+		calcArea.setEditable(false);
+		
+		
 		/*--------------------Create buttons etc. with properties in a pane--------------------*/
 		GridPane buttonPane = new GridPane();
+		mainPane.setBottom(buttonPane);
+		buttonPane.setAlignment(Pos.TOP_CENTER);
+		buttonPane.setStyle("-fx-background-color:#eeeeee; -fx-opacity:1; -fx-border-style: solid inside;");
 		buttonPane.setPadding(new Insets(10, 10, 10, 10));
 		buttonPane.setVgap(10);
-		buttonPane.setHgap(10);
+		buttonPane.setHgap(10);		
 		
+		// Set button size
+		setButtonSize(btClear);
+		setButtonSize(btAdd);
+		setButtonSize(btMinus);
+		setButtonSize(btMultiply);
+		setButtonSize(btDivide);
+		setButtonSize(btEquals);
+		setButtonSize(btLeftBracket);
+		setButtonSize(btRightBracket);
+		setButtonSize(btOne);
+		setButtonSize(btTwo);
+		setButtonSize(btThree);
+		setButtonSize(btFour);
+		setButtonSize(btFive);
+		setButtonSize(btSix);
+		setButtonSize(btSeven);
+		setButtonSize(btEight);
+		setButtonSize(btNine);
+		setButtonSize(btZero);
+
 		// Top row of the numbers
 		buttonPane.add(btSeven, 1, 1);
 		buttonPane.add(btEight, 2, 1);
@@ -79,20 +113,61 @@ public class Calculator extends Application {
 		buttonPane.add(btDivide, 4, 4);
 		buttonPane.add(btLeftBracket, 5, 1);
 		buttonPane.add(btRightBracket, 5, 2);
-
 		
 		// Bottom row of operators
 		buttonPane.add(btClear, 1, 4);
 		buttonPane.add(btZero, 2, 4);
 		buttonPane.add(btEquals, 3, 4);
 		
-		mainPane.setCenter(buttonPane);
+		// Radio button grouping
+		rbInfix.setToggleGroup(tgFixGroup);
+		rbPostfix.setToggleGroup(tgFixGroup);
+		rbInfix.setStyle("-fx-font-size: 12pt;");
+		rbPostfix.setStyle("-fx-font-size: 12pt;");
+		buttonPane.add(rbInfix, 5, 3);
+		buttonPane.add(rbPostfix, 5, 4);
+		
+		
+		/*--------------------Button actions--------------------*/
 
-		btEquals.setOnAction(e -> calculate());
-		btClear.setOnAction(e -> clearTextFields());
+		// Number button actions
+		btOne.setOnAction(e -> btOnePress());
+		btTwo.setOnAction(e -> btTwoPress());
+		btThree.setOnAction(e -> btThreePress());
+		btFour.setOnAction(e -> btFourPress());
+		btFive.setOnAction(e -> btFivePress());
+		btSix.setOnAction(e -> btSixPress());
+		btSeven.setOnAction(e -> btSevenPress());
+		btEight.setOnAction(e -> btEightPress());
+		btNine.setOnAction(e -> btNinePress());
+		btZero.setOnAction(e -> btZeroPress());
+
+		// Operator Button actions
+		btClear.setOnAction(e -> btClearPress());
+		btAdd.setOnAction(e -> btAddPress());
+		btMinus.setOnAction(e -> btMinusPress());
+		btMultiply.setOnAction(e -> btMultiplyPress());
+		btDivide.setOnAction(e -> btDividePress());
+		btEquals.setOnAction(e -> btEqualsPress());
+		btLeftBracket.setOnAction(e -> btLeftBracketPress());
+		btRightBracket.setOnAction(e -> btRightBracketPress());
+		
+		
+		/*--------------------Create radio button functionality--------------------*/
+		rbInfix.setOnAction(e -> {
+				if(rbInfix.isSelected()) {
+					System.out.println("Operating with infix...");
+				}
+		});
+		rbPostfix.setOnAction(e -> {
+			if(rbPostfix.isSelected()) {
+				System.out.println("Operating with postfix...");
+			}
+		});
+		
 		
 		/*--------------------Scene and stage--------------------*/
-		Scene scene = new Scene(mainPane, 300, 300);
+		Scene scene = new Scene(mainPane, 400, 300);
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.setTitle("JavaFX Calculator");
@@ -100,12 +175,71 @@ public class Calculator extends Application {
 
 	}
 	
-	private double calculate() {
-		return 0;
+	
+	/*--------------------Create number button functionality--------------------*/
+	private void btOnePress() {
+		calcArea.appendText("1");
+	}
+	private void btTwoPress() {
+		calcArea.appendText("2");
+	}
+	private void btThreePress() {
+		calcArea.appendText("3");
+	}
+	private void btFourPress() {
+		calcArea.appendText("4");
+	}
+	private void btFivePress() {
+		calcArea.appendText("5");
+	}
+	private void btSixPress() {
+		calcArea.appendText("6");
+	}
+	private void btSevenPress() {
+		calcArea.appendText("7");
+	}
+	private void btEightPress() {
+		calcArea.appendText("8");
+	}
+	private void btNinePress() {
+		calcArea.appendText("9");
+	}
+	private void btZeroPress() {
+		calcArea.appendText("0");
+	}
+	
+	
+	/*--------------------Create operator button functionality--------------------*/
+	private void btAddPress() {
+		calcArea.appendText("+");
+	}
+	private void btMinusPress() {
+		calcArea.appendText("-");
+	}
+	private void btMultiplyPress() {
+		calcArea.appendText("*");
+	}
+	private void btDividePress() {
+		calcArea.appendText("/");
+	}
+	private void btLeftBracketPress() {
+		calcArea.appendText("(");
+	}
+	private void btRightBracketPress() {
+		calcArea.appendText(")");
+	}
+	private void btEqualsPress() {
+		System.out.println("CALCULATING");
+	}
+	private void btClearPress() {
+		calcArea.setText("");
 	}
 
-	private void clearTextFields() {
-		calcArea.setText("");
+	
+	/*--------------------Utility functions--------------------*/
+	private void setButtonSize(Button bt) {
+		bt.setPrefSize(40, 40);
+		bt.setStyle("-fx-font-size: 12pt;");
 	}
 
 	public static void main(String[] args) {
